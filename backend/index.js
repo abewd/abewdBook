96aefa0613f2401f88c2eafa43d7baa61eacad10
Abewd <116496332+abewd@users.noreply.github.com>
2023-04-17 00:48:20 +1000

    added code to index.js, added the API key, and added react files and npm i for them and added the chatspage and the authpage

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,33 @@ const express = require("express");
 
 // Install cors to call the server from any other origin
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 // Install express framework
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-// Create random username
+// Create random pw for authentication
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      {
+        username: username,
+        secret: username,
+        first_name: username,
+      },
+      { headers: { "private-key": "12528c2d-b9b0-4979-9591-2ea973128fcf" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    console.error("Error:", e.message); // Log the error message
+    console.error("Error response data:", e.response.data); // Log the error response data
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
 
 // Run port here
--- backend/thingsIdid.md
+++ backend/thingsIdid.md
@@ -9,3 +9,18 @@
 
 4. make nodemon run our node
    go to script in package json and add a attribute start and make the script in two quotes "nodemon index.js"
+
+5. copied and pasted some code from here:
+   https://blog.chatengine.io/fullstack-chat/nodejs-reactjs
+
+6. npm run start to test in index.js
+
+7. install rest client to test this all that its generating the usernames, this is the same shit as insomnia but its inbuilt, saves time innit
+
+Youll be able to see the fake pw be generated in this too when you run it in REST client
+
+8. now lets add proper authentication using chat engine io
+
+rest.chatengine.io has all REST commands you need on there, just copy paste
+
+9.
